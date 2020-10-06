bc6e8286ac1a1b29183d728f1c918dd77bda9efc
rasuloff789 <rasuloff789@gmail.com>
2020-10-06 11:28:44 +0500

    all js and another and

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var openButton =document.querySelector('.hamburger');
-openButton.addEventListener('click',function(){
+var openNav = document.querySelector('.site-header__sitenav')
 
+var openHeader = document.querySelector('.site-header__container')
+openButton.addEventListener('click',function(){
+  openHeader.classList.toggle('open-btn');
+  openNav.classList.toggle('site-header__sitenav--open');
 openButton.classList.toggle('is-active') ;
 
 });
