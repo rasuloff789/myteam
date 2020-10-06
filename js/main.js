97b83a76cd8b43a77542e5836a35c10a0e2a5e0c
rasuloff789 <rasuloff789@gmail.com>
2020-10-06 11:34:52 +0500

    reindent lines hero and site header

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ var openHeader = document.querySelector('.site-header__container')
 openButton.addEventListener('click',function(){
   openHeader.classList.toggle('open-btn');
   openNav.classList.toggle('site-header__sitenav--open');
-openButton.classList.toggle('is-active') ;
-
+  openButton.classList.toggle('is-active') ;
+  
 });
