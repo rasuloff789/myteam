d35de2ed1dc0427643f296e311d65a84d4b1bb0f
rasuloff789 <rasuloff789@gmail.com>
2020-10-07 20:56:41 +0500

    regular js directors end

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ openButton.addEventListener('click',function(){
   openButton.classList.toggle('is-active') ;
   
 });
+
+var directorButton = document.querySelector('.director__button');
+
+var frontBox = document.querySelector('.director__front');
+
+var backBox = document.querySelector('.director__back');
+
+directorButton.addEventListener('click' , function(){
+  directorButton.classList.toggle('director__button--open');
+  frontBox.classList.toggle('director__front--open');
+  backBox.classList.toggle('director__back--open');
+})
